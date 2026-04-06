370a20c4641acd00cfc41d063eec2cbdce863772
Bikash <vikashraja2059@gmail.com>
2026-04-06 11:52:01 +0200

    feat: add PieceType enum class in model layer

--- src/main/java/com/bikashraja/chess/model/PieceType.java
+++ src/main/java/com/bikashraja/chess/model/PieceType.java
@@ -1,4 +1,29 @@
 package com.bikashraja.chess.model;
 
-public class PieceType {
+/**
+ * Represents the six types of chess pieces.
+ *
+ * We use standard centipawn value for each piece used in most of the chess engines.
+ *
+ * We use 100000 for the King, high enough that losing it always outweighs
+ * any other gain.
+ */
+public enum PieceType {
+    PAWN(100),
+    KNIGHT(320),
+    BISHOP(330),
+    ROOK(500),
+    QUEEN(900),
+    KING(100000);
+
+    // The material value of pieces in centipawns.
+    private final int value;
+
+    PieceType(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
 }
