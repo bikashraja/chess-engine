2481cb5195b990770009061ec4be4b4add473c65
Bikash <vikashraja2059@gmail.com>
2026-04-06 11:33:11 +0200

    feat: add Color enum class in model layer

--- src/main/java/com/bikashraja/chess/model/Color.java
+++ src/main/java/com/bikashraja/chess/model/Color.java
@@ -1,4 +1,19 @@
 package com.bikashraja.chess.model;
 
-public class Color {
+/**
+ * Represents the two players in chess game.
+ *
+ * Using enum helps to flip between two possible colors after each move.
+ */
+public enum Color {
+    WHITE,
+    BLACK;
+
+    /**
+     *
+     * @return the opposite color.
+     */
+    public Color opposite() {
+        return this == WHITE ? BLACK : WHITE;
+    }
 }
