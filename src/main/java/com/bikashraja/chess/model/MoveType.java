e102d7ce934147cf29d40f062165fb82181f9419
Bikash <vikashraja2059@gmail.com>
2026-04-07 17:02:55 +0200

    feat: add MoveType enum class in model layer

---
+++ src/main/java/com/bikashraja/chess/model/MoveType.java
@@ -0,0 +1,11 @@
+package com.bikashraja.chess.model;
+
+/**
+ * Represent normal and special move types in chess.
+ */
+public enum MoveType {
+    NORMAL,
+    CASTLING,
+    EN_PASSANT,
+    PROMOTION
+}
