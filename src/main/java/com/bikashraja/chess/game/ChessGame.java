b09b82f0d1b942fe2ce1a67519dba3849dd361c1
Bikash <vikashraja2059@gmail.com>
2026-04-28 19:47:36 +0200

    feat: implement helper methods for move application in ChessGame

--- src/main/java/com/bikashraja/chess/game/ChessGame.java
+++ src/main/java/com/bikashraja/chess/game/ChessGame.java
@@ -2,8 +2,13 @@ package com.bikashraja.chess.game;
 
 import com.bikashraja.chess.engine.MoveGenerator;
 import com.bikashraja.chess.model.Board;
+import com.bikashraja.chess.model.Color;
 import com.bikashraja.chess.model.GameState;
 import com.bikashraja.chess.model.Move;
+import com.bikashraja.chess.model.MoveType;
+import com.bikashraja.chess.model.Piece;
+import com.bikashraja.chess.model.PieceType;
+import com.bikashraja.chess.model.Position;
 
 import java.util.List;
 import java.util.Objects;
@@ -20,6 +25,11 @@ import java.util.Objects;
  * packages.</p>
  */
 public class ChessGame {
+
+    private static final Position WHITE_QUEENSIDE_ROOK_START = new Position(7, 0); // a1
+    private static final Position WHITE_KINGSIDE_ROOK_START  = new Position(7, 7); // h1
+    private static final Position BLACK_QUEENSIDE_ROOK_START = new Position(0, 0); // a8
+    private static final Position BLACK_KINGSIDE_ROOK_START  = new Position(0, 7); // h8
 
     private Board board;
     private GameState gameState;
@@ -90,8 +100,112 @@ public class ChessGame {
         this.gameState = newGameState;
     }
 
-    private GameState updateGameState(Board oldBoard, GameState oldGameState, Move move) {
-        // TODO
-        throw new UnsupportedOperationException("TODO: implement updateGameState");
+    private GameState updateGameState(Board oldBoard, GameState oldState, Move move) {
+        Piece movedPiece = oldBoard.getPiece(move.getFrom());
+        if (movedPiece == null) {
+            throw new IllegalStateException("No piece found on move source square: " + move.getFrom());
+        }
+
+        Piece capturedPiece = getCapturedPiece(oldBoard, move, movedPiece);
+
+        boolean whiteKingSide = oldState.canWhiteCastleKingSide();
+        boolean whiteQueenSide = oldState.canWhiteCastleQueenSide();
+        boolean blackKingSide = oldState.canBlackCastleKingSide();
+        boolean blackQueenSide = oldState.canBlackCastleQueenSide();
+
+        Position newEnPassantTarget = null;
+
+        // Update castling rights based on moved piece
+        if (movedPiece.getType() == PieceType.KING) {
+            if (movedPiece.getColor() == Color.WHITE) {
+                whiteKingSide = false;
+                whiteQueenSide = false;
+            } else {
+                blackKingSide = false;
+                blackQueenSide = false;
+            }
+        }
+
+        if (movedPiece.getType() == PieceType.ROOK) {
+            Position from = move.getFrom();
+
+            if (from.equals(WHITE_KINGSIDE_ROOK_START)) {
+                whiteKingSide = false;
+            } else if (from.equals(WHITE_QUEENSIDE_ROOK_START)) {
+                whiteQueenSide = false;
+            } else if (from.equals(BLACK_KINGSIDE_ROOK_START)) {
+                blackKingSide = false;
+            } else if (from.equals(BLACK_QUEENSIDE_ROOK_START)) {
+                blackQueenSide = false;
+            }
+        }
+
+        // Update castling rights if rook is captured on its home square
+        if (capturedPiece != null && capturedPiece.getType() == PieceType.ROOK) {
+            Position captureSquare = getCapturedPiecePosition(move, movedPiece);
+
+            if (captureSquare.equals(WHITE_KINGSIDE_ROOK_START)) {
+                whiteKingSide = false;
+            } else if (captureSquare.equals(WHITE_QUEENSIDE_ROOK_START)) {
+                whiteQueenSide = false;
+            } else if (captureSquare.equals(BLACK_KINGSIDE_ROOK_START)) {
+                blackKingSide = false;
+            } else if (captureSquare.equals(BLACK_QUEENSIDE_ROOK_START)) {
+                blackQueenSide = false;
+            }
+        }
+
+        // Update en passant target
+        if (isPawnDoubleStep(movedPiece, move)) {
+            int middleRow = (move.getFrom().getRow() + move.getTo().getRow()) / 2;
+            int col = move.getFrom().getCol();
+            newEnPassantTarget = new Position(middleRow, col);
+        }
+
+        // Switch side to move
+        Color nextSideToMove = oldState.getSideToMove().opposite();
+
+        return new GameState(
+                nextSideToMove,
+                whiteKingSide,
+                whiteQueenSide,
+                blackKingSide,
+                blackQueenSide,
+                newEnPassantTarget
+        );
+    }
+
+    private Piece getCapturedPiece(Board oldBoard, Move move, Piece movedPiece) {
+        // For en passant, the captured pawn is not on move.getTo()
+        if (isEnPassantMove(move, movedPiece)) {
+            Position capturedPawnPosition = getEnPassantCapturedPawnPosition(move);
+            return oldBoard.getPiece(capturedPawnPosition);
+        }
+
+        return oldBoard.getPiece(move.getTo());
+    }
+
+    private Position getCapturedPiecePosition(Move move, Piece movedPiece) {
+        if (isEnPassantMove(move, movedPiece)) {
+            return getEnPassantCapturedPawnPosition(move);
+        }
+        return move.getTo();
+    }
+
+    private boolean isEnPassantMove(Move move, Piece movedPiece) {
+        return movedPiece.getType() == PieceType.PAWN
+                && move.getMoveType() == MoveType.EN_PASSANT;
+    }
+
+    private Position getEnPassantCapturedPawnPosition(Move move) {
+        int capturedPawnRow = move.getFrom().getRow();
+        int capturedPawnCol = move.getTo().getCol();
+        return new Position(capturedPawnRow, capturedPawnCol);
+    }
+
+    private boolean isPawnDoubleStep(Piece movedPiece, Move move) {
+        return movedPiece.getType() == PieceType.PAWN
+                && Math.abs(move.getFrom().getRow() - move.getTo().getRow()) == 2
+                && move.getFrom().getCol() == move.getTo().getCol();
     }
 }
