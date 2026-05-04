7245e8e851739c42eaf2825c997286fcc42c5291
Bikash <vikashraja2059@gmail.com>
2026-05-04 19:55:33 +0200

    feat: implement board rendering with Unicode piece symbols

--- src/main/java/com/bikashraja/chess/ui/BoardRenderer.java
+++ src/main/java/com/bikashraja/chess/ui/BoardRenderer.java
@@ -1,11 +1,30 @@
 package com.bikashraja.chess.ui;
 
 import com.bikashraja.chess.model.Board;
+import com.bikashraja.chess.model.Piece;
+import com.bikashraja.chess.model.Position;
 
 public class BoardRenderer {
 
     public void render(Board board) {
-        // TODO: print board to terminal
-        throw new UnsupportedOperationException("TODO: implement board rendering");
+        for (int row = 0; row < 8; row++) {
+            int rank = 8 - row;
+            System.out.print(rank + " ");
+
+            for (int col = 0; col < 8; col++) {
+                Position pos = new Position(row, col);
+                Piece piece = board.getPiece(pos);
+
+                if (piece == null) {
+                    System.out.print(". ");
+                } else {
+                    System.out.print(piece.getSymbol() + " ");
+                }
+            }
+
+            System.out.println();
+        }
+
+        System.out.println("  a b c d e f g h");
     }
 }
